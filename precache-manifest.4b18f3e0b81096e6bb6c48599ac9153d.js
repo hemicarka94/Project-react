7ad0c9be5e471c67f8e8e1f014498e050ea169eb
hemicarka94 <pantelic.micika.milica@gmail.com>
2020-09-25 10:47:56 +0200

    Updates

---
+++ precache-manifest.4b18f3e0b81096e6bb6c48599ac9153d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7510c65dfff539a4a7f8acb260b810",
+    "url": "/Project-react/index.html"
+  },
+  {
+    "revision": "5bf9769cab105c3e9a3f",
+    "url": "/Project-react/static/css/main.92660700.chunk.css"
+  },
+  {
+    "revision": "e31d967e955310b6c318",
+    "url": "/Project-react/static/js/2.e24611c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project-react/static/js/2.e24611c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf9769cab105c3e9a3f",
+    "url": "/Project-react/static/js/main.e395429e.chunk.js"
+  },
+  {
+    "revision": "85ee719777893b70219d",
+    "url": "/Project-react/static/js/runtime-main.103b1f3a.js"
+  }
+]);
